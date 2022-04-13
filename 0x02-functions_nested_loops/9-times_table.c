09c4296e2ff57d94446f4301c2f5ef70f0e8ee36
Jifar71 <jifarraya3@gmail.com>
2022-04-13 16:43:06 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,8 +4,10 @@
  * times_table - fuction that prints the 9 times table, starting with 0
  * Return: Always 0
  */
-void times_table(void)
+int main(void)
 {
+times_table(void)
+
 int num, mult, prod;
 
 for (num = 0; num <= 9; num++)
