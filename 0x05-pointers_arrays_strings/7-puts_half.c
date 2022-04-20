641b57a01e404892602dbd7a4f148e872574ab10
Jifar71 <jifarraya3@gmail.com>
2022-04-20 01:06:37 -0700

    My task 7 commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *puts_half - prints every character of the string
+ *@str: A pointer an int that will be changed 
+ *
+ *Return: void that means our answer is correct
+ */
+
+void pits_half(char *str)
+
+{
+int i, last;
+
+i = 0;
+while (str[i] != '\0')
+{
+i++
+}
+
+last = (i + 1) / 2;
+
+for (i = last; str[i]; i++)
+{
+_putchar (str[i]);
+}
+
+_putchar ('\n');
+
+}
